5ac01fea31254e0ed15eb7328c85b3d0bfe59ca0
Sachin Lovanshi <sachinlovanshi13@gmail.com>
2026-02-19 22:17:40 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const authRoutes = require("./routes/authRoutes");
+
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Socket connection
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+});
+
+app.get("/", (req, res) => {
+    res.send("Vital Hope API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
